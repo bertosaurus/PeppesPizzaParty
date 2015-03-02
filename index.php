ba6386821301a3245adb9aa1e06bdf6d878a5b56
Albin <Albin@Albin-Dator.lan>
2015-03-02 21:07:38 +0100

    #2

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="index.css" rel="stylesheet">
+  </head>
+  <body>
+      
+      <div class="container">
+          
+          <div class="row">
+              
+              <div class="col-lg-3">
+                  
+                  
+              </div>
+              <div class="col-lg-6 flip-circle">
+                  
+                  <a href="#"><img src="img/pizza.jpg" class="img-responsive" alt="Responsive image"></div></a>
+                  
+              </div>
+              <div class="col-lg-3">
+                  
+                  
+              </div>
+              
+          </div>
+          
+      </div>
 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ js/egen.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".flip-circle a").click(function() { 
+    
+    
+    });
+    });
