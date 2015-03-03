c648d85e6c611abb61fd10ed72c0aec71c92c7b6
Albin <Albin@Albin-Dator.lan>
2015-03-03 23:48:57 +0100

    #3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Peppes Pizza Party</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -12,31 +12,37 @@
   </head>
   <body>
       
+      
       <div class="container">
           
-          <div class="row">
+<!--          <div class="row meny-row">
               
-              <div class="col-lg-3">
-                  
-                  
-              </div>
-              <div class="col-lg-6 flip-circle">
-                  
-                  <a href="#"><img src="img/pizza.jpg" class="img-responsive" alt="Responsive image"></div></a>
-                  
-              </div>
-              <div class="col-lg-3">
-                  
-                  
-              </div>
+              <div class="col-lg-6 col-lg-offset-3 meny"></div>
               
-          </div>
-          
+          </div>-->
+          <div class="row circle-row">
+              <div class="col-lg-6 col-lg-offset-3 meny-framsida">
+                  <button class="btn">hej</button>
+              </div>
+              <div class="col-lg-6 col-lg-offset-3 meny-framsida-2">
+                  
+              </div>
+              <div class="col-lg-12 flip-circle">                 
+                   <section class="container circle">
+                       <div id="card">
+                    <figure class="front"></figure>
+                    <figure class="back"></figure>
+                </div>
+            </section>
+                  <!--<a href="#"><img src="img/pizza.jpg" class="img-responsive" alt="Responsive image"></div></a>-->
+              </div>                         
+          </div>          
       </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/egen.js"></script>
   </body>
 </html>
--- js/egen.js
+++ js/egen.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-    $(".flip-circle a").click(function() { 
-    
-    
+    $("figure.front").click(function() {
+        $("#card").toggleClass("flipped")
+        console.log("klick körs. Knapp gömd.");
+
+        $(this).parent().parent().parent().parent().children(".meny-framsida")
+                .delay(2500).animate({top: 920}, 3000).delay(1000).animate({left: 300}, 1000);
+        $(this).parent().parent().parent().parent().children(".meny-framsida-2")
+                .delay(2500).animate({top: 920}, 3000).delay(1000).animate({left: 300}, 1000);
     });
+    
+    $(".btn").click(function() {
+        
+        $(".meny-framsida").toggleClass("flipped-meny")
+        
     });
+});
