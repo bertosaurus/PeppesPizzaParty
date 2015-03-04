ce56b818ca03ffb2ee139ec806f54bc307ebd6ba
Albin <Albin@Albin-Dator.lan>
2015-03-04 23:15:35 +0100

    #4

--- index.php
+++ index.php
@@ -1,48 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Peppes Pizza Party</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Peppes Pizza Party</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="index.css" rel="stylesheet">
-  </head>
-  <body>
-      
-      
-      <div class="container">
-          
-<!--          <div class="row meny-row">
-              
-              <div class="col-lg-6 col-lg-offset-3 meny"></div>
-              
-          </div>-->
-          <div class="row circle-row">
-              <div class="col-lg-6 col-lg-offset-3 meny-framsida">
-                  <button class="btn">hej</button>
-              </div>
-              <div class="col-lg-6 col-lg-offset-3 meny-framsida-2">
-                  
-              </div>
-              <div class="col-lg-12 flip-circle">                 
-                   <section class="container circle">
-                       <div id="card">
-                    <figure class="front"></figure>
-                    <figure class="back"></figure>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="index.css" rel="stylesheet">
+    </head>
+    <body>
+
+
+        <div class="container">
+
+            <!--          <div class="row meny-row">
+                          
+                          <div class="col-lg-6 col-lg-offset-3 meny"></div>
+                          
+                      </div>-->
+            <div class="row circle-row">
+                <div class="col-lg-12 meny-framsida-div">
+                    <section class="col-lg-6 col-lg-offset-3 container meny-framsida-container">
+                        <div class="meny-framsida">
+                            <figure class="meny-framsida-front"><h1>front</h1></figure>
+                            <figure class="meny-framsida-back"><h1>back</h1></figure>
+                        </div>
+                    </section>
                 </div>
-            </section>
-                  <!--<a href="#"><img src="img/pizza.jpg" class="img-responsive" alt="Responsive image"></div></a>-->
-              </div>                         
-          </div>          
-      </div>
+                
+                <div class="col-lg-12 meny-forstasida-div">
+                    <section class="col-lg-6 col-lg-offset-3 container meny-forstasida-container">
+                        <div class="meny-forstasida">
+                            <figure class="meny-forstasida-front"><h1>front</h1></figure>
+                            <figure class="meny-forstasida-back"><h1>back</h1></figure>
+                        </div>
+                    </section>
+                </div>
+                
+                <div class="col-lg-12 meny-andrasida-div">
+                    <section class="col-lg-6 col-lg-offset-3 container meny-andrasida-container">
+                        <div class="meny-andrasida">
+                            <figure class="meny-andrasida-front"><h1>front</h1></figure>
+                            <figure class="meny-andrasida-back"><h1>back</h1></figure>
+                        </div>
+                    </section>
+                </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/egen.js"></script>
-  </body>
+                <div class="col-lg-12 flip-circle">                 
+                    <section class="container circle">
+                        <div id="card">
+                            <figure class="front"></figure>
+                            <figure class="back"></figure>
+                        </div>
+                    </section>
+                          <!--<a href="#"><img src="img/pizza.jpg" class="img-responsive" alt="Responsive image"></div></a>-->
+                </div>                         
+            </div>          
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/egen.js"></script>
+    </body>
 </html>
--- js/egen.js
+++ js/egen.js
@@ -3,15 +3,50 @@ $(document).ready(function() {
         $("#card").toggleClass("flipped")
         console.log("klick körs. Knapp gömd.");
 
-        $(this).parent().parent().parent().parent().children(".meny-framsida")
-                .delay(2500).animate({top: 920}, 3000).delay(1000).animate({left: 300}, 1000);
-        $(this).parent().parent().parent().parent().children(".meny-framsida-2")
-                .delay(2500).animate({top: 920}, 3000).delay(1000).animate({left: 300}, 1000);
+        $(this).parent().parent().parent().parent().children(".meny-framsida-div")
+                .delay(2500).animate({top: 920}, 3000)
+        $(this).parent().parent().parent().parent().children(".meny-forstasida-div")
+                .delay(2500).animate({top: 919}, 3000)
+        $(this).parent().parent().parent().parent().children(".meny-andrasida-div")
+                .delay(2500).animate({top: 919}, 3000)
     });
     
-    $(".btn").click(function() {
+//    framsida
+    
+    $(".meny-framsida-front").click(function() {
         
-        $(".meny-framsida").toggleClass("flipped-meny")
+        $(".meny-framsida-container").animate({left: 275}, 1000);
+        $(".meny-framsida").toggleClass("meny-flipped");
+        $(".meny-forstasida-container").animate({left: 275}, 1000);
+        $(".meny-andrasida-container").animate({left: 275}, 1000);
         
     });
+    $(".meny-framsida-back").click(function() {
+        
+        $(".meny-framsida-container").animate({left: 0}, 1000);
+        $(".meny-framsida").toggleClass("meny-flipped").delay(1000).animate({left: 300}, 1000);
+        $(".meny-forstasida-container").animate({left: 0}, 1000);
+        $(".meny-andrasida-container").animate({left: 0}, 1000);
+        
+    });
+    
+//    förstasida
+    
+    $(".meny-forstasida-front").click(function() {
+        
+        $(".meny-forstasida-container").animate({left: 275}, 1000);
+        $(".meny-forstasida").toggleClass("meny-flipped");
+//        $(".meny-andrasida-container").animate({left: 275}, 1000);
+        
+    });
+    $(".meny-forstasida-back").click(function() {
+        
+        $(".meny-forstasida-container").animate({left: 0}, 1000);
+        $(".meny-forstasida").toggleClass("meny-flipped").delay(1000).animate({left: 300}, 1000);
+//        $(".meny-andrasida-container").animate({left: 0}, 1000);
+        
+    });
+    
+//    andrasida
+    
 });
