d97ae1e9a46f289b9e3ddc38054627ac5dc6a750
Albin <Albin@Albin-Dator.lan>
2015-04-05 17:37:09 +0200

    #13

--- footer.inc.php
+++ footer.inc.php
@@ -1,7 +1,24 @@
 
 <div class="footerSpacer"></div>
 <div class="footerWrapper">
-    
-    <a>hej</a>
-    
+
+    <div class="footerKonInf">
+        <h3>Kontakt Information</h3>
+        <p>Tel: 070-4201337</p>
+        <p>Mail: Peppe.PpP@gmail.com</p>
+    </div>
+
+    <div class="footerimg">
+        <img src="http://placehold.it/100x100">
+    </div>
+
+    <div class="footerSocMed">
+        <h3>Sociala Medier</h3>
+        <div class="SocMedImg">
+        <a href ="https://www.facebook.com/"><img src="img/facebookicon.png" width="50px" height="50px" class="img-responsive" alt="Responsive image"></a>
+        <a href ="http://instagram.com/"><img src="img/instagramicon.png" width="50px" height="50px" class="img-responsive" alt="Responsive image"></a>
+        <a href ="https://twitter.com/?lang=sv"><img src="img/twittericon.png" class="img-responsive" alt="Responsive image"></a>
+        </div>
+    </div>
+
 </div>
--- forstaSidaFront.inc.php
+++ forstaSidaFront.inc.php
@@ -11,15 +11,29 @@
     <div class="forstaSidapeppeTextFront">
 
         <div class="peppe-header">
-            <div peppe-header-img>
+            <div class="peppe-header-img">
                 <img src="http://placehold.it/125x125">
             </div>
             <div class="peppe-header-text">
-            <h1>Dj Peppe</h1>
+                <h1>Peppe?</h1>
             </div>
         </div>
 
         <div class="peppe-section">
+
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <br>         
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 
         </div>
 
